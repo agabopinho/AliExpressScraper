4e7e25f8f5b60a493c84b4399941ff693d28e616
Ágabo D. de Pinho <agabo.duarte@hotmail.com>
2016-09-20 11:22:26 -0300

    Update folders

--- AliScraperConsole/Properties/AssemblyInfo.cs
+++ AliScraperConsole/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AliScraperConsole")]
-[assembly: AssemblyCopyright("Copyright © ISAASOFT 2015")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
